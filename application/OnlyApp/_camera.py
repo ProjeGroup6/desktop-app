d3794238833c22feba6241a36e0e82fa47278cf9
Yusuf Arslan <yusufarsln98@outlook.com>
2023-07-17 03:39:28 +0300

    image processing added

--- application/OnlyApp/_camera.py
+++ application/OnlyApp/_camera.py
@@ -1,17 +1,56 @@
 from PyQt5.QtGui import QImage
 from PyQt5.QtCore import QThread
 from PyQt5 import QtCore, QtWidgets
+import os
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+import cv2
+import numpy as np
+
 
 class RunThread(QtCore.QThread):
+    CUSTOM_MODEL_NAME = "my_ssd_mobnet"
+    LABEL_MAP_NAME = "label_map.pbtxt"
+
+    paths = {
+        "ANNOTATION_PATH": os.path.join("Tensorflow", "workspace", "annotations"),
+        "CHECKPOINT_PATH": os.path.join(
+            "Tensorflow", "workspace", "models", CUSTOM_MODEL_NAME
+        ),
+    }
+
+    files = {
+        "PIPELINE_CONFIG": os.path.join(
+            "Tensorflow", "workspace", "models", CUSTOM_MODEL_NAME, "pipeline.config"
+        ),
+        "LABELMAP": os.path.join(paths["ANNOTATION_PATH"], LABEL_MAP_NAME),
+    }
+
     changePixmap = QtCore.pyqtSignal(QImage)
+    configs = config_util.get_configs_from_pipeline_file(files["PIPELINE_CONFIG"])
+    detection_model = model_builder.build(
+        model_config=configs["model"], is_training=False
+    )
 
     def __init__(self, sock, parent=None):
         super(RunThread, self).__init__(parent)
         self.sock = sock
 
+    @tf.function
+    def detect_fn(self, image):
+        image, shapes = self.detection_model.preprocess(image)
+        prediction_dict = self.detection_model.predict(image, shapes)
+        detections = self.detection_model.postprocess(prediction_dict, shapes)
+        return detections
+
     def run(self):
-        import cv2  # Import the cv2 module here
-        import numpy as np  # Import numpy module here
+        ckpt = tf.compat.v2.train.Checkpoint(model=self.detection_model)
+        ckpt.restore(
+            os.path.join(self.paths["CHECKPOINT_PATH"], "ckpt-3")
+        ).expect_partial()
 
         print(self.sock)
         while True:
@@ -30,47 +69,43 @@ class RunThread(QtCore.QThread):
             frame = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
             height, width, _ = frame.shape
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            category_index = label_map_util.create_category_index_from_labelmap(
+                self.files["LABELMAP"]
+            )
 
-            # l_h = cv2.getTrackbarPos("L-H", "Trackbars")
-            # l_s = cv2.getTrackbarPos("L-S", "Trackbars")
-            # l_v = cv2.getTrackbarPos("L-V", "Trackbars")
-            # u_h = cv2.getTrackbarPos("U-H", "Trackbars")
-            # u_s = cv2.getTrackbarPos("U-S", "Trackbars")
-            # u_v = cv2.getTrackbarPos("U-V", "Trackbars")
+            input_tensor = tf.convert_to_tensor(
+                np.expand_dims(frame, 0), dtype=tf.float32
+            )
+            detections = self.detect_fn(input_tensor)
 
-            # lower_red = np.array([l_h, l_s, l_v])
-            # upper_red = np.array([u_h, u_s, u_v])
+            num_detections = int(detections.pop("num_detections"))
+            detections = {
+                key: value[0, :num_detections].numpy()
+                for key, value in detections.items()
+            }
+            detections["num_detections"] = num_detections
 
-            # mask = cv2.inRange(hsv, lower_red, upper_red)
-            # kernel = np.ones((5, 5), np.uint8)
-            # mask = cv2.erode(mask, kernel)
+            # detection_classes should be ints.
+            detections["detection_classes"] = detections["detection_classes"].astype(
+                np.int64
+            )
 
-            # # Contours detection
-            # if int(cv2.__version__[0]) > 3:
-            #     # Opencv 4.x.x
-            #     contours, hierarchy = cv2.findContours(
-            #         mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-            #     )
-            # else:
-            #     # Opencv 3.x.x
-            #     _, contours, hierarchy = cv2.findContours(
-            #         mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-            #     )
+            label_id_offset = 1
+            frame_with_detections = frame.copy()
 
-            # if hierarchy is not None:
-            #     hierarchy = hierarchy[0]  # get the first contour
+            viz_utils.visualize_boxes_and_labels_on_image_array(
+                frame_with_detections,
+                detections["detection_boxes"],
+                detections["detection_classes"] + label_id_offset,
+                detections["detection_scores"],
+                category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=5,
+                min_score_thresh=0.8,
+                agnostic_mode=False,
+            )
 
-            #     for component in zip(contours, hierarchy):
-            #         current_contour = component[0]
-            #         current_hierarchy = component[1]
-            #         x, y, w, h = cv2.boundingRect(current_contour)
-            #         area = cv2.contourArea(current_contour)
-            #         if current_hierarchy[3] < 0 and area > 10000:
-            #             # these are the outermost parent components
-            #             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            # else:
-            #     print("No contours found")
-
-            image = QImage(frame.data, width, height, QImage.Format_RGB888)
+            image = QImage(
+                frame_with_detections.data, width, height, QImage.Format_RGB888
+            )
             self.changePixmap.emit(image)
--- application/lidar_app.py
+++ application/lidar_app.py
@@ -1,59 +1,115 @@
-import pickle
 import sys
 import socket
-import struct
-import threading
-import json
 import numpy as np
 import pyqtgraph as pg
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from pyqtgraph import ScatterPlotItem
+import fcntl
 
-point_num = 360
-port = 8082
+port = 8080
 added_points = set()
 scatter = None
 plot_widget = None
+mapfile = "map.txt"
+transfile = "transform.txt"
 
 
-def receive_points(socket_conn):
-    data = socket_conn.recv(4096).decode()
-    if not data:
-        print("not")
-        return
+# def receive_points(socket_conn):
+#     data = socket_conn.recv(4096).decode()
+#     if not data:
+#         print("not")
+#         return
 
-    try:
-        data = json.loads(data)
-        print(data)
-    except pickle.UnpicklingError as e:
-        print("error")
-        return  # or any other appropriate error handling
+#     try:
+#         data = json.loads(data)
+#         print(data)
+#     except pickle.UnpicklingError as e:
+#         print("error")
+#         return  # or any other appropriate error handling
 
-    # # traverse data in for loop
-    # for i in range(len(data)):
-    #     if data[i] == None:
-    #         continue
-    #     # get x and y with angle and distance
-    #     x = int(data[i] * np.cos(np.deg2rad(i)) * 10) / 10
-    #     y = int(data[i] * np.sin(np.deg2rad(i)) * 10) / 10
-    #     # Check if the point already exists
-    #     if (x, y) not in added_points:
-    #         # Add the point to the scatter plot item
-    #         if x > 0 and y > 0:
-    #             brush = pg.mkBrush(
-    #                 0, 255, 0, 120
-    #             )  # Green color for points in the positive quadrant
-    #         else:
-    #             brush = pg.mkBrush(
-    #                 (255, 0, 0, 120)
-    #             )  # Red color for points in the other quadrants
+#     # traverse data in for loop
+#     for i in range(len(data)):
+#         if data[i] == None:
+#             continue
+#         # get x and y with angle and distance
+#         x = int(data[i] * np.cos(np.deg2rad(i)) * 10) / 10
+#         y = int(data[i] * np.sin(np.deg2rad(i)) * 10) / 10
+#         # Check if the point already exists
+#         if (x, y) not in added_points:
+#             # Add the point to the scatter plot item
+#             if x > 0 and y > 0:
+#                 brush = pg.mkBrush(
+#                     0, 255, 0, 120
+#                 )  # Green color for points in the positive quadrant
+#             else:
+#                 brush = pg.mkBrush(
+#                     (255, 0, 0, 120)
+#                 )  # Red color for points in the other quadrants
 
-    #         scatter.addPoints([x], [y], brush=brush)
-    #         added_points.add((x, y))
+#             scatter.addPoints([x], [y], brush=brush)
+#             added_points.add((x, y))
 
-    #         # Update the plot range to fit all points
-    #         plot_widget.autoRange()
+#             # Update the plot range to fit all points
+#             plot_widget.autoRange()
+
+
+def read_points():
+    global mapfile, transfile
+    # the map file is in following format
+    # x1,y1
+    # x2, y2
+    # ....
+    # xn, yn
+
+    # read file
+    # data = np.loadtxt(mapfile, dtype=np.uint8)
+    # print(data)
+    with open(mapfile, "r") as file:
+        fcntl.flock(file, fcntl.LOCK_SH)
+        mapdata = file.read()
+        fcntl.flock(file, fcntl.LOCK_UN)
+
+    mapdata = mapdata.replace("\n", ",").split(",")
+    mapdata = [float(i) for i in mapdata if i]
+    mapdata = np.array(mapdata)
+    mapdata = mapdata.reshape(int(len(mapdata) / 2), 2)
+
+    # map data is in following format and it is a single line
+    # x, y, z, roll, pitch, yaw
+    # read file
+    with open(transfile, "r") as file:
+        fcntl.flock(file, fcntl.LOCK_SH)
+        transdata = file.read()
+        fcntl.flock(file, fcntl.LOCK_UN)
+
+    transdata = transdata.replace("\n", ",").split(",")
+    print(transdata)
+    # set x, y, z, roll, pitch and yaw
+    x = float(transdata[0]) + 1024
+    y = float(transdata[1]) + 1024
+    # yaw = float(transdata[5])
+
+    brush = pg.mkBrush(0, 255, 0, 120)  # Green color for self-tracking
+    added_points.add((x, y))
+    scatter.addPoints([x], [y], brush=brush)
+    scatter.addPoints([x - 1], [y], brush=brush)
+    scatter.addPoints([x + 1], [y], brush=brush)
+    scatter.addPoints([x], [y + 1], brush=brush)
+    scatter.addPoints([x], [y - 1], brush=brush)
+
+    # traverse the data
+    for i in range(len(mapdata)):
+        x = mapdata[i][0]
+        y = mapdata[i][1]
+
+        if (x, y) not in added_points:
+            # Add the point to the scatter plot item
+            brush = pg.mkBrush(255, 0, 0, 120)  # Red color for map
+            scatter.addPoints([x], [y], brush=brush)
+            added_points.add((x, y))
+            # Update the plot range to fit all points
+            plot_widget.autoRange()
 
 
 def plot_clicked(event):
@@ -64,16 +120,16 @@ def plot_clicked(event):
 
 
 def start_process():
-    global scatter, plot_widget
-    socket_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_conn.connect(("192.168.43.165", port))
+    global scatter, plot_widget, filename
+    # socket_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # socket_conn.connect(("192.168.43.165", port))
 
     app = QApplication(sys.argv)
     main_window = QMainWindow()
 
     # Create scatter plot item
     scatter = ScatterPlotItem(
-        size=10, pen=pg.mkPen(None), brush=pg.mkBrush(255, 0, 0, 120)
+        size=2, pen=pg.mkPen(None), brush=pg.mkBrush(255, 0, 0, 120)
     )
 
     # Create a plot widget and add the scatter plot item to it
@@ -92,7 +148,7 @@ def start_process():
 
     # Generate points in a loop
     timer = QTimer()
-    timer.timeout.connect(lambda: receive_points(socket_conn))
+    timer.timeout.connect(lambda: read_points())
     timer.start(1)  # Add a new point every second
 
     main_window.show()
